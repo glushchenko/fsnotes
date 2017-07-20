a71876eb4e71b56cad499f0e9349afb623817701
Oleksandr Glushchenko <fluder@gmail.com>
2017-07-20 20:14:10 +0300

    Initial Commit

---
+++ FSNotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FSNotes
+//
+//  Created by Oleksandr Glushchenko on 7/20/17.
+//  Copyright © 2017 Oleksandr Glushchenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FSNotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FSNotes
+//
+//  Created by Oleksandr Glushchenko on 7/20/17.
+//  Copyright © 2017 Oleksandr Glushchenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
