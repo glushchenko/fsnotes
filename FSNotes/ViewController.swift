daf4e16f95e1381aa15d8c91470c9a11c1e67d70
Oleksandr Glushchenko <fluder@gmail.com>
2017-07-20 20:17:06 +0300

    Init

--- FSNotes/ViewController.swift
+++ FSNotes/ViewController.swift
@@ -8,20 +8,39 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTextViewDelegate,
+NSTextFieldDelegate {
+    
+    @IBOutlet var textView: NSTextView!
+    @IBOutlet weak var search: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        textView.delegate = self
+        search.delegate = self
+        textView.textContainerInset = NSMakeSize(0, 5);
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    func textDidChange(_ notification: Notification) {
+        print(textView.string)
+    }
+    
+    override func controlTextDidChange(_ obj: Notification) {
+        print(search.stringValue)
+    }
+    
+    @IBAction func demo(_ sender: NSTextField) {
+        print(222)
+    }
 }
 
