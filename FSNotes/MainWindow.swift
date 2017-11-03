eb5f513025eae7c054221c9c0dff5f1c7f687815
Oleksandr Glushchenko <fluder@gmail.com>
2017-11-03 15:06:55 +0200

    Xcode 9.1

---
+++ FSNotes/MainWindow.swift
@@ -0,0 +1,9 @@
+//
+//  MainWindow.swift
+//  FSNotes
+//
+//  Created by Oleksandr Glushchenko on 11/2/17.
+//  Copyright © 2017 Oleksandr Glushchenko. All rights reserved.
+//
+
+class MainWindow: NSWindow {}
---
+++ FSNotes/PrefsWindow.swift
@@ -0,0 +1,9 @@
+//
+//  PrefsWindow.swift
+//  FSNotes
+//
+//  Created by Oleksandr Glushchenko on 11/3/17.
+//  Copyright © 2017 Oleksandr Glushchenko. All rights reserved.
+//
+
+class PrefsWindow: NSWindow {}
