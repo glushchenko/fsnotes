bc3e5c612a8036c9395d7403d495b861d4a65e90
Jeff <jeff@marmaladesoul.com>
2018-03-25 19:08:31 +1300

    Format dates as time only when date is sometime today.

---
+++ FSNotes/DateFormatter+.swift
@@ -0,0 +1,28 @@
+//
+//  DateFormatter+.swift
+//  FSNotes
+//
+//  Created by Jeff Hanbury on 25/03/18.
+//  Copyright © 2018 Oleksandr Glushchenko. All rights reserved.
+//
+
+import Foundation
+
+
+extension DateFormatter {
+    func formatDateForDisplay(_ date: Date) -> String {
+        dateStyle = .short
+        timeStyle = .none
+        locale = NSLocale.autoupdatingCurrent
+        return string(from: date)
+    }
+    
+    func formatTimeForDisplay(_ date: Date) -> String {
+        dateStyle = .none
+        timeStyle = .short
+        locale = NSLocale.autoupdatingCurrent
+        return string(from: date)
+    }
+
+}
+
