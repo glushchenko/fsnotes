c4d6f45ff5524c15657e594e55264154df4c108d
buddax2 <buddax2@gmail.com>
2018-05-27 15:19:30 +0300

    additional fix for #248 (#256)
    Fixed an issue when start position is also a Unicode character with lengh > 1

--- FSNotes/View/SearchTextField.swift
+++ FSNotes/View/SearchTextField.swift
@@ -72,7 +72,7 @@ class SearchTextField: NSTextField, NSTextFieldDelegate {
         
         if allowAutocomplete && note.title.lowercased().starts(with: searchQuery.lowercased()) {
             stringValue = note.title
-            currentEditor()?.selectedRange = NSRange(searchQuery.count..<note.title.utf16.count)
+            currentEditor()?.selectedRange = NSRange(searchQuery.utf16.count..<note.title.utf16.count)
             allowAutocomplete = false
         }
     }
