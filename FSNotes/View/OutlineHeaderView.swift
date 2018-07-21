44d1835ca122b3318747cd0adec3c6cea759c619
Oleksandr Glushchenko <fluder@gmail.com>
2018-07-21 10:50:30 +0300

    Fixes sidebar scroll

---
+++ FSNotes/View/OutlineHeaderView.swift
@@ -0,0 +1,11 @@
+//
+//  OutlineHeaderView.swift
+//  FSNotes
+//
+//  Created by Oleksandr Glushchenko on 7/21/18.
+//  Copyright © 2018 Oleksandr Glushchenko. All rights reserved.
+//
+
+import Cocoa
+
+class OutlineHeaderView: NSVisualEffectView {}
