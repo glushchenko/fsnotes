1d045f147f739a43cb049d14f81442699adc3ba8
Oleksandr Glushchenko <fluder@gmail.com>
2019-02-15 20:05:46 +0200

    - Tiny print refactoring

---
+++ FSNotes/ViewController+Print.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController+Print.swift
+//  FSNotes
+//
+//  Created by Oleksandr Glushchenko on 2/15/19.
+//  Copyright © 2019 Oleksandr Glushchenko. All rights reserved.
+//
+
+import WebKit
+import cmark_gfm_swift
+
+extension ViewController {
+
+    @objc func allImagesLoaded(_ message: String) {}
+
+    override class func isSelectorExcluded(fromWebScript sel: Selector) -> Bool {
+        if sel == #selector(ViewController.allImagesLoaded(_:)) {
+            return false
+        }
+        return true
+    }
+
+    override class func webScriptName(for sel: Selector) -> String? {
+        if sel == #selector(ViewController.allImagesLoaded(_:)) {
+            return "allImagesLoaded";
+        }
+        return nil
+    }
+
+    public func printMarkdownPreview(webView: WebView?) {
+        guard let note = EditTextView.note else { return }
+
+        let classBundle = Bundle(for: MarkdownView.self)
+        let url = classBundle.url(forResource: "DownView", withExtension: "bundle")!
+        let bundle = Bundle(url: url)!
+        let baseURL = bundle.url(forResource: "index", withExtension: "html")!
+
+        let markdownString = note.getPrettifiedContent()
+        let css = MarkdownView.getPreviewStyle(theme: "atom-one-light") + "  .copyCode { display: none; } body { -webkit-text-size-adjust: none; font-size: 1.0em;} pre, code { border: 1px solid #c0c4ce; border-radius: 3px; } pre, pre code { word-wrap: break-word; }";
+
+        var template = try! NSString(contentsOf: baseURL, encoding: String.Encoding.utf8.rawValue)
+        template = template.replacingOccurrences(of: "DOWN_CSS", with: css) as NSString
+
+        let html = Node(markdown: markdownString)!.html
+        var htmlString = template.replacingOccurrences(of: "DOWN_HTML", with: html)
+        var imagesStorage = note.project.url
+
+        if note.type == .TextBundle {
+            imagesStorage = note.url
+        }
+
+        htmlString = self.loadImages(imagesStorage: imagesStorage, html: htmlString)
+
+        webView?.frameLoadDelegate = self
+        webView?.mainFrame.loadHTMLString(htmlString, baseURL: baseURL)
+
+        if UserDataService.instance.isDark {
+            webView?.stringByEvaluatingJavaScript(from: "switchToDarkMode();")
+        }
+    }
+
+    func webView(_ sender: WebView!, didFinishLoadFor frame: WebFrame!) {
+        if sender.isLoading {
+            return
+        }
+        if frame != sender.mainFrame {
+            return
+        }
+        if sender.stringByEvaluatingJavaScript(from: "document.readyState") == "complete" {
+            sender.frameLoadDelegate = nil
+
+            let printInfo = NSPrintInfo.shared
+            printInfo.paperSize = NSMakeSize(595.22, 841.85)
+            printInfo.topMargin = 40.0
+            printInfo.leftMargin = 40.0
+            printInfo.rightMargin = 40.0
+            printInfo.bottomMargin = 40.0
+
+            let when = DispatchTime.now() + 0.2
+            DispatchQueue.main.asyncAfter(deadline: when) {
+                let operation: NSPrintOperation = NSPrintOperation(view: sender.mainFrame.frameView.documentView, printInfo: printInfo)
+                operation.printPanel.options.insert(NSPrintPanel.Options.showsPaperSize)
+                operation.printPanel.options.insert(NSPrintPanel.Options.showsOrientation)
+                operation.run()
+            }
+        }
+    }
+
+    private func loadImages(imagesStorage: URL, html: String) -> String {
+        var htmlString = html
+
+        do {
+            let regex = try NSRegularExpression(pattern: "<img.*?src=\"([^\"]*)\"")
+            let results = regex.matches(in: html, range: NSRange(html.startIndex..., in: html))
+
+            let images = results.map {
+                String(html[Range($0.range(at: 0), in: html)!])
+            }
+
+            for image in images {
+
+                let localPath = image.replacingOccurrences(of: "<img src=\"", with: "").dropLast()
+
+                guard !localPath.starts(with: "http://") && !localPath.starts(with: "https://") else {
+                    continue
+                }
+
+                let fullImageURL = imagesStorage
+                let imageURL = fullImageURL.appendingPathComponent(String(localPath.removingPercentEncoding!))
+
+                var orientation = 0
+                let url = NSURL(fileURLWithPath: imageURL.path)
+                if let imageSource = CGImageSourceCreateWithURL(url, nil) {
+                    let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary?
+                    if let orientationProp = imageProperties?[kCGImagePropertyOrientation] as? Int {
+                        orientation = orientationProp
+                    }
+                }
+
+                let imageData = try Data(contentsOf: imageURL)
+                let base64prefix = "<img data-orientation=\"\(orientation)\" class=\"fsnotes-preview\" src=\"data:image;base64," + imageData.base64EncodedString() + "\""
+                htmlString = htmlString.replacingOccurrences(of: image, with: base64prefix)
+            }
+        } catch let error {
+            print("Images regex: \(error.localizedDescription)")
+        }
+
+        return htmlString
+    }
+}
--- FSNotes/ViewController.swift
+++ FSNotes/ViewController.swift
@@ -10,7 +10,6 @@ import Cocoa
 import MASShortcut
 import cmark_gfm_swift
 import FSNotesCore_macOS
-import Quartz.PDFKit
 import WebKit
 
 class ViewController: NSViewController,
@@ -32,6 +31,7 @@ class ViewController: NSViewController,
     var sidebarTimer = Timer()
     var rowUpdaterTimer = Timer()
     let searchQueue = OperationQueue()
+    var printWebView: WebView?
 
     override var representedObject: Any? {
         didSet { }  // Update the view, if already loaded.
@@ -84,7 +84,7 @@ class ViewController: NSViewController,
             }
         }
     }
-    
+
     // MARK: - Overrides
     
     override func viewDidLoad() {
@@ -913,7 +913,8 @@ class ViewController: NSViewController,
     
     @IBAction func printNotes(_ sender: NSMenuItem) {
         if let note = EditTextView.note, note.isMarkdown() {
-            printMarkdownPreview()
+            self.printWebView = WebView()
+            printMarkdownPreview(webView: self.printWebView)
             return
         }
 
@@ -933,129 +934,6 @@ class ViewController: NSViewController,
         operation.printPanel.options.insert(NSPrintPanel.Options.showsPaperSize)
         operation.printPanel.options.insert(NSPrintPanel.Options.showsOrientation)
         operation.run()
-    }
-
-    var webView: WebView?
-
-    public func printMarkdownPreview() {
-        guard let note = EditTextView.note else { return }
-
-        let classBundle = Bundle(for: MarkdownView.self)
-        let url = classBundle.url(forResource: "DownView", withExtension: "bundle")!
-        let bundle = Bundle(url: url)!
-        let baseURL = bundle.url(forResource: "index", withExtension: "html")!
-        let cssURL = bundle.url(forResource: "down", withExtension: "min.css", subdirectory: "css")
-
-        let markdownString = note.getPrettifiedContent()
-        let mainCSS = try! String(contentsOf: cssURL!)
-        let css = MarkdownView.getPreviewStyle(theme: "atom-one-light") + mainCSS + "  .copyCode { display: none; } body { -webkit-text-size-adjust: none; font-size: 1.0em;} pre, code { border: 1px solid #c0c4ce; border-radius: 3px; } pre, pre code { word-wrap: break-word; }";
-
-        var template = try! NSString(contentsOf: baseURL, encoding: String.Encoding.utf8.rawValue)
-        template = template.replacingOccurrences(of: "DOWN_CSS", with: css) as NSString
-
-        let html = Node(markdown: markdownString)!.html
-        var htmlString = template.replacingOccurrences(of: "DOWN_HTML", with: html)
-        var imagesStorage = note.project.url
-
-        if note.type == .TextBundle {
-            imagesStorage = note.url
-        }
-
-        htmlString = self.loadImages(imagesStorage: imagesStorage, html: htmlString)
-
-        self.webView = WebView()
-        self.webView?.frameLoadDelegate = self
-        self.webView?.mainFrame.loadHTMLString(htmlString, baseURL: baseURL)
-
-        if UserDataService.instance.isDark {
-            self.webView?.stringByEvaluatingJavaScript(from: "switchToDarkMode();")
-        }
-    }
-
-    override class func isSelectorExcluded(fromWebScript sel: Selector) -> Bool {
-        if sel == #selector(ViewController.allImagesLoaded(_:)) {
-            return false
-        }
-        return true
-    }
-
-    override class func webScriptName(for sel: Selector) -> String? {
-        if sel == #selector(ViewController.allImagesLoaded(_:)) {
-            return "allImagesLoaded";
-        }
-        return nil
-    }
-
-    @objc func allImagesLoaded(_ message: String) {}
-
-    func webView(_ sender: WebView!, didFinishLoadFor frame: WebFrame!) {
-        if sender.isLoading {
-            return
-        }
-        if frame != sender.mainFrame {
-            return
-        }
-        if sender.stringByEvaluatingJavaScript(from: "document.readyState") == "complete" {
-            sender.frameLoadDelegate = nil
-            guard let webView = self.webView else { return }
-
-            let printInfo = NSPrintInfo.shared
-            printInfo.paperSize = NSMakeSize(595.22, 841.85)
-            printInfo.topMargin = 40.0
-            printInfo.leftMargin = 40.0
-            printInfo.rightMargin = 40.0
-            printInfo.bottomMargin = 40.0
-
-            let when = DispatchTime.now() + 0.2
-            DispatchQueue.main.asyncAfter(deadline: when) {
-                let operation: NSPrintOperation = NSPrintOperation(view: webView.mainFrame.frameView.documentView, printInfo: printInfo)
-                operation.printPanel.options.insert(NSPrintPanel.Options.showsPaperSize)
-                operation.printPanel.options.insert(NSPrintPanel.Options.showsOrientation)
-                operation.run()
-            }
-        }
-    }
-
-    private func loadImages(imagesStorage: URL, html: String) -> String {
-        var htmlString = html
-
-        do {
-            let regex = try NSRegularExpression(pattern: "<img.*?src=\"([^\"]*)\"")
-            let results = regex.matches(in: html, range: NSRange(html.startIndex..., in: html))
-
-            let images = results.map {
-                String(html[Range($0.range(at: 0), in: html)!])
-            }
-
-            for image in images {
-
-                let localPath = image.replacingOccurrences(of: "<img src=\"", with: "").dropLast()
-
-                guard !localPath.starts(with: "http://") && !localPath.starts(with: "https://") else {
-                    continue
-                }
-
-                let fullImageURL = imagesStorage
-                let imageURL = fullImageURL.appendingPathComponent(String(localPath.removingPercentEncoding!))
-
-                var orientation = 0
-                let url = NSURL(fileURLWithPath: imageURL.path)
-                if let imageSource = CGImageSourceCreateWithURL(url, nil) {
-                    let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary?
-                    if let orientationProp = imageProperties?[kCGImagePropertyOrientation] as? Int {
-                        orientation = orientationProp
-                    }
-                }
-
-                let imageData = try Data(contentsOf: imageURL)
-                let base64prefix = "<img data-orientation=\"\(orientation)\" class=\"fsnotes-preview\" src=\"data:image;base64," + imageData.base64EncodedString() + "\""
-                htmlString = htmlString.replacingOccurrences(of: image, with: base64prefix)
-            }
-        } catch let error {
-            print("Images regex: \(error.localizedDescription)")
-        }
-
-        return htmlString
     }
 
     @IBAction func openProjectViewSettings(_ sender: NSMenuItem) {
@@ -1854,4 +1732,3 @@ class ViewController: NSViewController,
     }
 
 }
-
