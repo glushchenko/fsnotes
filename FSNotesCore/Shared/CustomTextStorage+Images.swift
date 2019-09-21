e7c36db1ff35dd3fc8d47078c4be7ce5eada00a9
Oleksandr Hlushchenko <fluder@icloud.com>
2019-09-21 12:55:22 +0300

    Images loading speedup

---
+++ FSNotesCore/Shared/CustomTextStorage+Images.swift
@@ -0,0 +1,9 @@
+//
+//  CustomTextStorage+Images.swift
+//  FSNotes iOS
+//
+//  Created by Олександр Глущенко on 9/21/19.
+//  Copyright © 2019 Oleksandr Glushchenko. All rights reserved.
+//
+
+import Foundation
