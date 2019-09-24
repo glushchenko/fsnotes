bde52f1519599f2101d21f1e791a0e025ad9a05d
Oleksandr Hlushchenko <fluder@icloud.com>
2019-09-24 18:39:35 +0300

    Revert ruined pull request

--- FSNotes/View/NoteCellView.swift
+++ FSNotes/View/NoteCellView.swift
@@ -27,7 +27,7 @@ class NoteCellView: NSTableCellView {
     private var previewMaximumLineHeight: CGFloat = 12
     private let previewLineSpacing: CGFloat = 3
 
-    public weak var tableView: NotesTableView? {
+    public var tableView: NotesTableView? {
         get {
             guard let vc = ViewController.shared() else { return nil }
             
