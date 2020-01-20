503766bea69588262ee54e61a1bca32d51c171fe
Oleksandr Yakubchyk <buddax2@gmail.com>
2020-01-20 21:49:18 +0200

    Fix for #765: Change default value for Text Replacement from False to True

--- FSNotes/Helpers/UserDefaultsManagement.swift
+++ FSNotes/Helpers/UserDefaultsManagement.swift
@@ -806,7 +806,7 @@ public class UserDefaultsManagement {
             if let result = UserDefaults.standard.object(forKey: Constants.AutomaticTextReplacement) {
                 return result as! Bool
             }
-            return false
+            return true
         }
         set {
             UserDefaults.standard.set(newValue, forKey: Constants.AutomaticTextReplacement)
