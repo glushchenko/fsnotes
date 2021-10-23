0203b922869ad0861e9403f423fcb856e8b151a7
Oleksandr Hlushchenko <fluder@icloud.com>
2021-10-23 08:40:15 +0300

    #1226

--- FSNotes/ViewController.swift
+++ FSNotes/ViewController.swift
@@ -183,6 +183,9 @@ class ViewController: NSViewController,
 
         if sidebarOutlineView.isFirstLaunch, let x = UserDefaultsManagement.lastScreenX, let y = UserDefaultsManagement.lastScreenY {
             view.window?.setFrameOrigin(NSPoint(x: x, y: y))
+
+            UserDefaultsManagement.lastScreenX = nil
+            UserDefaultsManagement.lastScreenY = nil
         }
 
         if UserDefaultsManagement.fullScreen {
