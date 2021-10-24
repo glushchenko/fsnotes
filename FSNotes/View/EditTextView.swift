f2716f3944915c48710659f66cb3092890f055df
Oleksandr Hlushchenko <fluder@icloud.com>
2021-10-24 12:35:12 +0300

    - Fixes macOS Monterey nstextview invalidation

--- FSNotes/View/EditTextView.swift
+++ FSNotes/View/EditTextView.swift
@@ -835,6 +835,7 @@ class EditTextView: NSTextView, NSTextFinderClient, NSSharingServicePickerDelega
     }
 
     func fill(note: Note, highlight: Bool = false, saveTyping: Bool = false, force: Bool = false) {
+        textStorage?.setAttributedString(NSAttributedString(string: ""))
 
         // Hack for invalidate prev layout data (order is important, only before fill)
         if let length = textStorage?.length {
