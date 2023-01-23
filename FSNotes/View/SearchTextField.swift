fc55c6867bc2d5b76b910ebfbc75a5737b5a93d9
glushchenko <fluder@icloud.com>
2023-01-23 20:32:34 +0200

    #1459

--- FSNotes/View/SearchTextField.swift
+++ FSNotes/View/SearchTextField.swift
@@ -71,6 +71,10 @@ class SearchTextField: NSSearchField, NSSearchFieldDelegate {
         self.lastQueryLength = 0
 
         addRecent(query: stringValue)
+        
+        if stringValue.count == 0 {
+            search()
+        }
     }
 
     override func keyUp(with event: NSEvent) {
