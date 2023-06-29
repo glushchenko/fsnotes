483671beb4702d62be4d53b4b9b1923151d7eb73
Nikita Demidov <nikitademidov@192.168.1.174>
2023-06-29 23:16:04 +0100

    check if project is selected before notes reload

--- FSNotes iOS/View/SidebarTableView.swift
+++ FSNotes iOS/View/SidebarTableView.swift
@@ -136,9 +136,12 @@ class SidebarTableView: UITableView,
 
         selectRow(at: indexPath, animated: false, scrollPosition: .none)
         vc.configureNavMenu(for: sidebarItem)
+        
+        guard vc.searchQuery.project != newQuery.project else { return }
 
         vc.reloadNotesTable(with: newQuery) {
             DispatchQueue.main.async {
+                
                 vc.setNavTitle(folder: name)
 
                 guard vc.notesTable.notes.count > 0 else {
