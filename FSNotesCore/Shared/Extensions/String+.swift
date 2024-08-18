d6696cfb459a185929451b149a33bd88f27a4c0a
glushchenko <fluder@icloud.com>
2024-08-18 19:00:15 +0300

    - Reverts md5 implemetation

--- FSNotesCore/Shared/Extensions/String+.swift
+++ FSNotesCore/Shared/Extensions/String+.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import CryptoKit
+import CommonCrypto
 
 #if os(OSX)
 import Cocoa
@@ -83,8 +83,13 @@ public extension String {
     }
 
     var md5: String {
-        let computed = Insecure.MD5.hash(data: self.data(using: .utf8)!)
-        return computed.map { String(format: "%02hhx", $0) }.joined()
+        let data = Data(self.utf8)
+        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
+            return hash
+        }
+        return hash.map { String(format: "%02x", $0) }.joined()
     }
 
     var isWhitespace: Bool {
